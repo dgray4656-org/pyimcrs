3f7fb85f119b20478adb5d60a1c7adba47c74e12
Dave Gray <dgray4656@yahoo.com>
2019-06-09 21:26:36 -0400

    added new 'url' to setup.py: https://github.com/dgray4656-org/pyimcrs

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Python package for interacting with HR iMC restful services",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/dgray4656-org/pyimcrs",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
