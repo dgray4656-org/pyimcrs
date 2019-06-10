ee635c05b8634c3be2a449e6d24107822363ecc3
Dave Gray <dgray4656@yahoo.com>
2019-06-09 21:28:08 -0400

    corrected typo- 'HP' and not 'HR'

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.1",
     author="David E. Gray",
     author_email="dgray4656@yahoo.com",
-    description="Python package for interacting with HR iMC restful services",
+    description="Python package for interacting with HP iMC restful services",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dgray4656-org/pyimcrs",
