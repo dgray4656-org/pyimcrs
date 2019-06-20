1c9655d79f24ceffa42bc6b739c6902663df492e
Dave Gray <dgray4656@yahoo.com>
2019-06-19 21:27:28 -0400

    updated version to 0.0.1a updated __init__.py to import ImcSession and constants from utils.py added install_requires to setup.py specifying requests>=2.22.0

--- pyimcrs/__init__.py
+++ pyimcrs/__init__.py
@@ -1,1 +1,5 @@
 name="pyimcrs"
+
+from .session import ImcSession
+from .util import SYS_SETTINGS,NO_FILTER,FILTER
+from .util import BASE_URL,DEVICE_PATH,MONITOR_PATH,THRESHOLD_PATH,ALARM_PATH
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyimcrs",
-    version="0.0.1",
+    version="0.0.1a",
     author="David E. Gray",
     author_email="dgray4656@yahoo.com",
     description="Python package for interacting with HP iMC restful services",
@@ -13,7 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dgray4656-org/pyimcrs",
     packages=setuptools.find_packages(),
-    classifiers=[
+    install_requires=[
+          'requests>=2.22.0',
+      ],
+	classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
