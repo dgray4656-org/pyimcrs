68c94bd2adab08a25362a1540956270702e4b1f4
Dave Gray <dgray4656@yahoo.com>
2019-06-22 08:29:31 -0400

    removed unnecessary whitespace

--- pyimcrs/device.py
+++ pyimcrs/device.py
@@ -4,7 +4,7 @@ class ImcDevice():
 	"""Class to represent Device managed by IMC
 		properties TBD
 	"""
-	
+
 	def __init__(self,id,label,ip,sysName,contact,location,sysOid,sysDescription):
 		self.id=id
 		self.label=label
@@ -14,8 +14,7 @@ class ImcDevice():
 		self.location=location
 		self.sysOid=sysOid
 		self.sysDescription=sysDescription
-		
-	
+
+
 	def spitout(self):
 		pprint(self)
-	
