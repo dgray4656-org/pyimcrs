b28dd83e301553cf647462db5610bc0625e24fda
Dave Gray <dgray4656@yahoo.com>
2020-04-07 20:35:10 -0400

    added get_alarm_list function

--- pyimcrs/session.py
+++ pyimcrs/session.py
@@ -16,6 +16,7 @@ class ImcSession():
 
 	def __init__(self, URL, u, p):
 		self.target = URL
+		self.user = u
 		self.s = requests.Session()
 		self.s.headers.update({'Content-type':'application/json','Accept':'application/json'})
 		self.s.verify=False
@@ -134,3 +135,99 @@ class ImcSession():
 
 			return output_list
 
+	def get_alarm_list(self, operatorName='a030147', total=False, start=0, size=10, orderby='faultTime', desc=False,
+					alarmLevel=None, recStatus=None, ackStatus=None, timeRange=None, startAlarmTime=None, endAlarmTime=None, startAlarmAckTime=None,
+					endAlarmAckTime=None, startAlarmDuration=None, endAlarmDuration=None, alarmDesc=None, descRelationship=None, acker=None,
+					userAcker=None, paras=None, customView=None, deviceIp=None, deviceId=None, ifIndex=None, id=None, alarmCategory=None,
+					faultOid=None, faultOidFuzzyQuery=False, originalType=None, holdInfo=None):
+		myparams={'operatorName':operatorName,
+				'total':total,
+				'start':start,
+				'size':size,
+				'orderby':orderby,
+				'desc':False}
+
+		if alarmLevel is not None:
+		    myparams.update({'alarmLevel':alarmLevel})
+		if recStatus is not None:
+		    myparams.update({'recStatus':recStatus})
+		if ackStatus is not None:
+		    myparams.update({'ackStatus':ackStatus})
+		if timeRange is not None:
+		    myparams.update({'timeRange':timeRange})
+		if startAlarmTime is not None:
+		    myparams.update({'startAlarmTime':startAlarmTime})
+		if endAlarmTime is not None:
+		    myparams.update({'endAlarmTime':endAlarmTime})
+		if startAlarmAckTime is not None:
+		    myparams.update({'startAlarmAckTime':startAlarmAckTime})
+		if endAlarmAckTime is not None:
+		    myparams.update({'endAlarmAckTime':endAlarmAckTime})
+		if startAlarmDuration is not None:
+		    myparams.update({'startAlarmDuration':startAlarmDuration})
+		if endAlarmDuration is not None:
+		    myparams.update({'endAlarmDuration':endAlarmDuration})
+		if alarmDesc is not None:
+		    myparams.update({'alarmDesc':alarmDesc})
+		if descRelationship is not None:
+		    myparams.update({'descRelationship':descRelationship})
+		if acker is not None:
+		    myparams.update({'acker':acker})
+		if userAcker is not None:
+		    myparams.update({'userAcker':userAcker})
+		if paras is not None:
+		    myparams.update({'paras':paras})
+		if customView is not None:
+		    myparams.update({'customView':customView})
+		if deviceIp is not None:
+		    myparams.update({'deviceIp':deviceIp})
+		if deviceId is not None:
+		    myparams.update({'deviceId':deviceId})
+		if ifIndex is not None:
+		    myparams.update({'ifIndex':ifIndex})
+		if id is not None:
+		    myparams.update({'id':id})
+		if alarmCategory is not None:
+		    myparams.update({'alarmCategory':alarmCategory})
+		if faultOid is not None:
+		    myparams.update({'faultOid':faultOid})
+		if faultOidFuzzyQuery is not False:
+		    myparams.update({'faultOidFuzzyQuery':faultOidFuzzyQuery})
+		if originalType is not None:
+		    myparams.update({'originalType':originalType})
+		if holdInfo is not None:
+		    myparams.update({'holdInfo':holdInfo})
+
+		output_list=[]
+
+		myparams.update({'total':True})
+
+		target=self.target + util.ALARM_PATH
+		
+		try:
+			response=self.s.get(target,params=myparams)
+			count = response.headers['Total']
+		except Exception as e:
+			print(e)
+			count = 0
+
+		if count==0:	
+			return outputlist
+		else:
+			myparams.update({'total':False})
+			myparams.update({'size':count})
+			try:
+				response = self.s.get(target,params=myparams)
+				jres = response.json()
+				jres2 = jres.get('data')
+			except Exception as e:
+				print(e)
+				return output_list
+
+			if type(jres2) is dict:
+				output_list.append(jres2)
+			elif type(jres2) is list:
+				for x in jres2:
+					output_list.append(x)
+
+			return output_list
